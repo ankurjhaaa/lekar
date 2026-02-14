c86e95e6cdee07b4fa348a5fb3003dca3cbfc27e
ankurjhaaa <kumarankur7258@gmail.com>
2026-02-15 03:58:29 +0530

    refactor: Migrate Vite configuration and main application entry point to TypeScript.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
     @viteReactRefresh
-    @vite(['resources/js/app.jsx', "resources/js/pages/{$page['component']}.jsx"])
+    @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.jsx"])
     @inertiaHead
 </head>
 
